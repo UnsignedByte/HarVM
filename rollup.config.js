fa04232145c5aa8ab8402b0d840d7da0b2f8bbb7
UnsignedByte <edlam1889@gmail.com>
2020-05-24 23:32:23 -0700

    use rollup

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+/*
+* @Author: UnsignedByte
+* @Date:   16:10:21, 24-May-2020
+* @Last Modified by:   UnsignedByte
+* @Last Modified time: 23:30:08, 24-May-2020
+*/
+
+export default {
+  input: 'src/main.js',
+  output: {
+    file: 'build/main.js',
+    format: 'iife',
+    name: 'Bot'
+  }
+};
