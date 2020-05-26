6b8e169375a9b08c9623fbc443670250978619cb
UnsignedByte <edlam1889@gmail.com>
2020-05-26 00:48:51 -0700

    mov esave

--- src/utils/data_manager.js
+++ src/utils/data_manager.js
@@ -2,7 +2,7 @@
 * @Author: UnsignedByte
 * @Date:   11:56:39, 25-May-2020
 * @Last Modified by:   UnsignedByte
-* @Last Modified time: 00:45:05, 26-May-2020
+* @Last Modified time: 00:48:46, 26-May-2020
 */
 
 class DataManager {
@@ -16,12 +16,12 @@ class DataManager {
 	} 
 
 	get({def, args}, raw=this.raw){
-		this.#save();
 		def = {} || def
 		return (args.length===0 && raw) || this.get({ def, args:args.slice(1) }, (args[0] in raw && args[0]) || (raw[args[0]] = def))
 	}
 
 	set({def, args}, value){
+		this.#save();
 		this.get({def, args:args.slice(0, args.length-1)})[args[args.length-1]] = value;
 	}
 }
