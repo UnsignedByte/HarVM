1c0d5769e8307a21add6453cecbf332e4c91378e
SheepTester <ussa.say@gmail.com>
2020-08-04 21:10:04 -0700

    Make dumb shipping commands for edgy teens (because owobot had them)

--- src/commands/user.js
+++ src/commands/user.js
@@ -1,4 +1,4 @@
-import { BashlikeArgumentParser } from '../utils/parsers.js'
+import { BashlikeArgumentParser, SimpleArgumentParser } from '../utils/parsers.js'
 import * as resolve from '../utils/client-resolve.js'
 
 async function dm ({ args: { message, recipient }, client, msg }) {
@@ -45,10 +45,44 @@ executor.parser = new BashlikeArgumentParser([
 	}
 ], 'Store the @mention of the user running the command in a variable. Example: `user executor -> me`.')
 
+function compat ({ args: { userA, userB }, msg, reply, trace }) {
+	userA = resolve.member(msg, userA)
+	if (!userA) return { message: `Could not resolve the first person; are they on this server?`, trace }
+	userB = resolve.member(msg, userB)
+	if (!userB) return { message: `Could not resolve the second person; are they on this server?`, trace }
+	// I can tolerate the loss of precision here from converting snowflakes to
+	// floats
+	const userAId = +userA.id
+	const userBId = +userB.id
+	reply(`${userA} and ${userB} are ${
+		(1 / (Math.abs(userAId - userBId) / 2e17 + 1) * 100).toFixed(2)
+	}% compatible!`)
+}
+compat.parser = new SimpleArgumentParser({
+	main: '<userA> <userB>'
+}, null, 'Determines how compatible two people are with each other.')
+
+function ship ({ args: { userA, userB }, msg, reply, trace }) {
+	userA = resolve.member(msg, userA)
+	if (!userA) return { message: `Could not resolve the first person; are they on this server?`, trace }
+	userB = resolve.member(msg, userB)
+	if (!userB) return { message: `Could not resolve the second person; are they on this server?`, trace }
+	reply(`${userA} x ${userB} -> 💞 **${
+		// `slice` rounds down
+		userA.displayName.slice(0, userA.displayName.length / 2) +
+			userB.displayName.slice(userB.displayName.length / 2)
+	}**`)
+}
+ship.parser = new SimpleArgumentParser({
+	main: '<userA> <userB>'
+}, null, 'Generate a portmanteau name for a couple. (This is not a commutative operation.)')
+
 export {
 	dm,
-	executor
+	executor,
+	compat,
+	ship
 }
 export default function main ({ reply }) {
-	return reply('`user [` `dm` | `executor` `]` ...')
+	return reply('`user [` `dm` | `executor` | `compat` | `ship` `]` ...')
 }
